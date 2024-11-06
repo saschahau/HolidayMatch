d34b33204c11fd651b616993de6f2c64968dceb0
Linchen752 <alina.borsari@gmx.ch>
2024-11-06 09:01:19 +0100

    Created great function

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
-
+def great(text):
+    st.write(f"Hello {text}")
 def app():
     st.title('Welcome to HolidayMatch')
     st.write('Your AI-powered travel assistant!')
     st.write('Stay tuned for updates...')
+    great("Alina")
 
 if __name__ == '__main__':
     app()
